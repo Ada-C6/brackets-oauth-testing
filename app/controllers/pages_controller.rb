7def574ced09a0de4d2ff28fbeacb232597ee8af
Dan Roberts <dan@adadevelopersacademy.org>
2016-10-20 19:12:38 -0700

    Get most page view stuff working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,20 +3,53 @@ class PagesController < ApplicationController
   end
 
   def new
+    @page = Page.new
   end
 
   def create
+    @page = Page.new(page_params)
+    @page.user_id = session[:user_id]
+    if @page.save
+      redirect_to page_path(@page)
+    else
+      render :new
+    end
   end
 
   def show
+    @page = find_page
   end
 
   def edit
+    @page = find_page
   end
 
   def update
+    @page = find_page
+    @page.assign_attributes(page_params)
+    if @page.save
+      redirect_to page_path(@page)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @page = find_page
   end
+
+private
+    def page_params
+      params.require(:page).permit(:title, :content)
+    end
+
+    # Could be a model method, but because it's got the render
+    # I think it fits better here.
+    def find_page
+      begin
+        return Page.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render file: "public/404", status: :not_found
+      end
+    end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,7 @@
 module PagesHelper
+  def conditional_page_link(page, **kwargs)
+    if !page.id.nil?
+      link_to "Return to Page", page, **kwargs
+    end
+  end
 end
