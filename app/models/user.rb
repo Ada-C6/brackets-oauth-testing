7a002b0166a7f7a702aaa064f4a44a49f610ec29
Dan Roberts <dan@adadevelopersacademy.org>
2016-10-20 17:09:03 -0700

    Show logged in user at top of every page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ActiveRecord::Base
   has_many :pages
-  
+
   validates :uid, presence: true, uniqueness: { scope: :provider,
     message: "Cannot have multiple users from the same provider with the same UID"}
-  validates :provider, presence: true
-end
+    validates :provider, presence: true
+
+    def self.build_from_github(auth_hash)
+      user       = User.new
+      user.uid   = auth_hash[:uid]
+      user.provider = 'github'
+      user.name  = auth_hash['info']['name']
+      user.email = auth_hash['info']['email']
+
+      return user
+    end
+  end
